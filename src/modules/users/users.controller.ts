f3b3a19dba46f80f9ad06fa526d6fdbd14f4e239
Ezequiel Sale <ezequielsale123@gmail.com>
2025-02-11 20:56:52 -0300

    se agrego paginacion al endpoint de getAllUsers

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -9,11 +9,13 @@ import {
   UseInterceptors,
   UploadedFile,
   Req,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { PaginationArgs } from 'src/utils/pagination/pagination.dto';
 
 @Controller('users')
 export class UsersController {
@@ -25,8 +27,8 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Query() pagination: PaginationArgs) {
+    return this.usersService.findAll(pagination);
   }
 
   @Get(':id')
