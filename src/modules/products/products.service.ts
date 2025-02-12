f5016a73eec51f7a9ba8258368fb70cf0fe0c000
georginacostilla <georginacostilla.gc@gmail.com>
2025-02-12 03:41:34 -0300

    se comenzo a trabajar sobre: products.service

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -5,25 +5,63 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  async create(newProduct: CreateProductDto) {
+    try {
+      const product = await this.prisma.product.create({
+        data: newProduct,
+      });
+      return product;
+    } catch (error) {
+      throw new Error(`Error al crear el producto: ${error.message}`);
+    }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    try {
+      const products = await this.prisma.product.findMany();
+      return products;
+    } catch (error) {
+      throw new Error('Error al obtener los productos');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    try {
+      const product = await this.prisma.product.findUnique({
+        where: { id },
+      });
+      return product;
+    } catch (error) {
+      throw new Error('Error al obtener el producto');
+    }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    try {
+      const updatedProduct = await this.prisma.product.update({
+        where: { id },
+        data: {
+          name: updateProductDto.name,
+          price: updateProductDto.price,
+          stock: updateProductDto.stock, // revisar categoria 
+        },
+      });
+      return updatedProduct;
+    } catch (error) {
+      throw new Error('Error al actualizar el producto');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    try {
+      const deletedProduct = await this.prisma.product.delete({
+        where: { id },
+      });
+      return deletedProduct;
+    } catch (error) {
+      throw new Error('Error al eliminar el producto');
+    }
   }
 }
