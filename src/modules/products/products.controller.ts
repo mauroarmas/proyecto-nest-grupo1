e8eb6b9ccbdac473ceb2f5799487f6475a6f4fc6
georginacostilla <georginacostilla.gc@gmail.com>
2025-02-15 03:35:03 -0300

    implementación de swagger en Productos y categorias

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -2,32 +2,104 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Crear un nuevo producto' })
+  @ApiBody({ type: CreateProductDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Producto creado exitosamente',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Datos inválidos en la solicitud',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Error interno del servidor',
+  })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Listar todos los productos' })
+  @ApiBody({ type: CreateProductDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de productos obtenida exitosamente',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Error interno del servidor',
+  })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Buscar un producto por ID' })
+  @ApiBody({ type: CreateProductDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Producto encontrado exitosamente',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Producto no encontrado',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Error interno del servidor',
+  })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar un producto' })
+  @ApiBody({ type: UpdateProductDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Producto actualizado exitosamente',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Datos inválidos en la solicitud',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Producto no encontrado',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Error interno del servidor',
+  })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un producto' })
+  @ApiBody({ type: CreateProductDto })
+  @ApiResponse({
+    status: 204,
+    description: 'Producto eliminado exitosamente',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Producto no encontrado',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Error interno del servidor',
+  })
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
