3328743eb631598706e29e79e7320e33b4452ebd
Luciano Casacci <lucianocasacci@Lucianos-MacBook-Air.local>
2025-02-28 17:28:19 -0300

    fix de carrito y swagger

---
+++ src/common/decorators/get-user.decorator.ts
@@ -0,0 +1,22 @@
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { User } from '@prisma/client';
+
+export const GetUser = createParamDecorator(
+    (data: keyof User | undefined, ctx: ExecutionContext): User | Partial<User> => {
+        const request = ctx.switchToHttp().getRequest();
+        const user = request.user;
+
+        if (!user) {
+            throw new UnauthorizedException('Usuario no encontrado en la solicitud');
+        }
+
+        if (data) {
+            if (!(data in user)) {
+                throw new UnauthorizedException(`La propiedad ${data} no existe en el usuario`);
+            }
+            return user[data];
+        }
+        
+        return user;
+    },
+); 
