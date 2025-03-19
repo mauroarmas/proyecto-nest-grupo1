538c1fb216cf5c8f3125b7e8866ded1ab76f667e
Ezequiel Sale <ezequielsale123@gmail.com>
2025-03-19 16:10:42 -0300

    se añadio la funcionalidad de iniciar sesion con google

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get, UseGuards, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RecoverPasswordDto, ResetPasswordDto } from './dto/auth.dto';
 import { LoginAuthDto } from './dto/login.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBody } from '@nestjs/swagger';
+import { GoogleAuthGuard } from './guards/google-auth.guard';
+import { Public } from 'src/common/decorators/public.decorator';
+import { JwtPayload } from 'src/common/interfaces';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -107,4 +110,48 @@ export class AuthController {
   ) {
     return await this.authService.resetPasswordWithToken(resetDto, token);
   }
+
+  @Public()
+  @UseGuards(GoogleAuthGuard)
+  @Get("google/login")
+  @ApiOperation({ summary: 'Google login' })
+  @ApiResponse({ status: 200, description: 'Redirect to Google login' })
+  googleLogin() {
+    
+  }
+
+  @Public()
+@UseGuards(GoogleAuthGuard)
+@Get('google/callback')
+async googleCallback(@Req() req, @Res() res) {
+  try {
+    const googleUser = req.user; // Usuario devuelto por Google
+
+    // Validar si el usuario existe o crearlo
+    const user = await this.authService.validateGoogleUser({
+      email: googleUser.email,
+      name: googleUser.name,
+      lastName: googleUser.familyName,
+      profileImg: googleUser.picture,
+    });
+
+    // Generar JWT
+    const payload: JwtPayload = {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    };
+
+    const token = await this.authService.createTokens(payload);
+
+    // Enviar respuesta con token y usuario al frontend
+    return res.redirect(
+      `http://localhost:3000?token=${token.accessToken}&id=${user.id}&role=${user.role}&email=${user.email}&name=${user.name}`
+    );
+  } catch (error) {
+    console.error('Error en googleCallback:', error);
+    return res.status(500).json({ message: 'Error en la autenticación con Google' });
+  }
+}
+
 }
---
+++ src/modules/auth/strategies/local.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      usernameField: 'email',
+    });
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    if (password === '')
+      throw new UnauthorizedException('Please enter a password');
+    return this.authService.login({ email, password });
+  }
+}
